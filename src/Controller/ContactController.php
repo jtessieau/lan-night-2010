51e8e6631f72d60e3efacf37b959bd4b6eb4ae06
Julien TESSIEAU <julien.tessieau@gmail.com>
2024-09-22 12:19:04 +0100

    📦 NEW: Add mail sending logic to the controller

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,25 +3,35 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\MailerService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(MailerService $mailer, Request $request): Response
     {
         $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // TODO: Add logic
+            try {
+                $mailer->sendContatFormEmail($form);
+                $this->addFlash('success','Message sent with success!');
+                
+                return $this->redirect($request->getUri());
+            } catch (\RuntimeException $e) {
+                $errorMessage = $e->getMessage();
+            }
         }
         return $this->render('pages/contact.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'errorMessage' => isset($errorMessage)? $errorMessage : null,
         ]);
     }
 }
